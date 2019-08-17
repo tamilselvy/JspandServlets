429d94b40537c595fd57a330b1f6dfee232318c8
tamilselvy <tamilselvysm@gmail.com>
2019-08-17 12:39:53 +0530

    EL expression

--- JspAndServlets/src/com/firstProject/FirstProject.java
+++ JspAndServlets/src/com/firstProject/FirstProject.java
@@ -10,44 +10,20 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class FirstProject extends HttpServlet {
-	public FirstProject(){
-		super();
-		System.out.println("inside the no-arg constructor()");
-	}
-
-	//important 2 methods- doGet and doPost
-//	@Override
-//	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-//		doPost(req, resp);
-//	}
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		Student st=new Student();
+		st.setName("Tamil");
+		
 		String firstName=req.getParameter("fname");
 		String lastName=req.getParameter("lname");
 		String fullName=firstName+lastName;
-//		System.out.println("My name is "+fullName);
-//		PrintWriter writer=resp.getWriter();
-//		writer.println("<html><body><h1>My name is "+fullName+"</h1></body></html>");
-//		resp.sendRedirect("https://www.google.com");
-//		resp.sendRedirect("test.html");
 		req.setAttribute("name", fullName);
+		req.setAttribute("st", st);
 		RequestDispatcher dispatcher=req.getRequestDispatcher("/NewFile.jsp");
 		dispatcher.forward(req ,resp);
 	}
-	@Override
-	public void init() throws ServletException {
-		super.init();
-		System.out.println("inside the init()");
-	}
-	@Override
-	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		super.service(req, resp);
-		System.out.println("inside the service()");
-	}
-	@Override
-	public void destroy() {
-		super.destroy();
-		System.out.println("inside the destroy()");
-	}
+	
 }
  
